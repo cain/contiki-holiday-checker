8090b17f21c848aed73d41f8a7f03da4565f7235
Cain Hall <cain.hall98@gmail.com>
2023-01-23 20:32:07 +1100

    update check api

--- api/check.js
+++ api/check.js
@@ -1,14 +1,7 @@
 import fetch from 'node-fetch';
 
 export default function handler(request, response) {
-  const res = await fetch('...', {
-    method: 'POST',
-    body: JSON.stringify({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+  const res = await fetch('https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=254196&startTimestamp=2023-06-01');
 
   const data = await res.json();
   return response.status(200).json({ data });
