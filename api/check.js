021868222afcd709da0a8dc10f528c1cc1de5759
Cain Hall <cain.hall98@gmail.com>
2023-01-23 21:03:36 +1100

    back to node fetch

--- api/check.js
+++ api/check.js
@@ -1,8 +1,11 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 export default function handler(request, response) {
-  axios.get('https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=254196&startTimestamp=2023-06-01')
-    .then((res) => {
-      return response.status(200).json({ res });
-    })
+  const res = await fetch('https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=254196&startTimestamp=2023-06-01', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  const data = await res.json();
+  return response.status(200).json({ data });
 }
