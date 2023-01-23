a2aec531d079447c0d2735fcbf93ad4976001dba
Cain Hall <cain.hall98@gmail.com>
2023-01-23 21:00:55 +1100

    update

--- api/check.js
+++ api/check.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default function handler(request, response) {
-  const res = await axios.get('https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=254196&startTimestamp=2023-06-01');
-
-  return response.status(200).json({ res });
+  axios.get('https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=254196&startTimestamp=2023-06-01')
+    .then((res) => {
+      return response.status(200).json({ res });
+    })
 }
