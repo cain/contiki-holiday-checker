f3a55bdfe5535698d36ddc5ea13c96342e3642c2
Cain Hall <cain.hall98@gmail.com>
2023-01-24 11:33:29 +1100

    find item

--- netlify/functions/check.js
+++ netlify/functions/check.js
@@ -4,6 +4,8 @@ const TOUR_OPTION = "254196";
 const TIME = "2023-06-01";
 const API_ENDPOINT = "https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=" +TOUR_OPTION + "&startTimestamp=" + TIME;
 
+const SELECTED = "2023-06-15"
+
 exports.handler = async (event, context) => {
   const response = fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
     .then((response) => response.json())
@@ -13,8 +15,10 @@ exports.handler = async (event, context) => {
     return response;
   }
 
+  const item = data.find((holiday) => holiday.startDate === SELECTED);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify(item),
   };
 };
