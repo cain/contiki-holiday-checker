20d904513d2c3d13535195f59d29028ce0645dc2
Cain Hall <cain.hall98@gmail.com>
2023-01-24 11:20:02 +1100

    contiki endpoint

--- netlify/functions/check.js
+++ netlify/functions/check.js
@@ -1,13 +1,15 @@
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const TOUR_OPTION = "254196";
+const TIME = "2023-06-01";
+const API_ENDPOINT = "https://www.contiki.com/en-au/tours/getdatespricing?tourOptionId=" +TOUR_OPTION + "&startTimestamp=" + TIME;
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
-      body: data.joke,
+      body: data,
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
