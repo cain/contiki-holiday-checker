690373ec8103cafcd7a7f10f1ff963b643b6c2db
Cain Hall <cain.hall98@gmail.com>
2024-10-30 14:57:25 +1100

    remove log

--- netlify/functions/coldplay.js
+++ netlify/functions/coldplay.js
@@ -22,7 +22,7 @@ exports.handler = async (event, context) => {
 
       const cleanedData = data.replace(/\$203\.54 ea • \d+ tickets/g, '').replace(/3 tickets are available/g, '');
 
-      console.log('cleanedData', cleanedData);
+      // console.log('cleanedData', cleanedData);
 
       const tickets = ['2 tickets', '3 tickets', '4 tickets', '5 tickets']
 
